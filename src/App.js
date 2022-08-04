cc94b7e7a285fecdb5f2269164130818898f3517
Alexandre Balata Souto <balatasouto@gmail.com>
2022-08-04 17:33:58 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import data from "./data";
 
 function App() {
+  function toggleText(e) {
+    const question = e.target.closest(".question");
+    let questionInfo = question.querySelector(".question_info");
+
+    if (questionInfo.style.maxHeight) {
+      questionInfo.style.maxHeight = null;
+    } else {
+      questionInfo.style.maxHeight = questionInfo.scrollHeight + "px";
+    }
+
+    // question.querySelector(".question_info").style.display =
+    //   question.querySelector(".question_info").style.display == "block"
+    //     ? "none"
+    //     : "block";
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <article className="container">
+      <h3 className="text">Questions And Answers About Login</h3>
+      <section>
+        {data.map((item) => {
+          return (
+            <div className="question" key={item.id}>
+              <div className="question_title">
+                <h3>{item.title}</h3>
+                <button onClick={toggleText} className="question_button">
+                  +
+                </button>
+              </div>
+              <div className="question_info">
+                <p>{item.info}</p>
+              </div>
+            </div>
+          );
+        })}
+      </section>
+    </article>
   );
 }
 
